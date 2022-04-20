990575904ceacb38156174a269cd962e5df14ab0
jasonshih <hlshih@gmail.com>
2022-04-20 08:24:58 +0000

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+export JAVA_HOME=/home/centos/jdk1.8.0_181-cloudera
+export PATH=$JAVA_HOME/bin:$PATH
+export MVN_HOME=/home/centos/apache-maven-3.8.5
+export PATH=$MVN_HOME/bin:$PATH
+
+#export IMPALA_HOME=/home/centos/apache-impala-3.4.0
+export IMPALA_HOME=/home/centos/impala-3.4.1
+export LANG=en_US.UTF-8
+
+export CLASSPATH=.:$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
+
+export PATH=/usr/local/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
